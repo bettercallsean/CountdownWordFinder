68d7dc0fc13b22efa8009eb081cdc89042aae8ba
Sean <seanedwards97@gmail.com>
2019-02-19 15:20:04 +0000

    Tidy up code

--- CountdownWordFinder/CountdownWordFinder.cs
+++ CountdownWordFinder/CountdownWordFinder.cs
@@ -9,43 +9,40 @@ namespace CountdownWordFinder
 {
     class CountdownWordFinder
     {
-        static Dictionary<string, HashSet<string>> dictionary = JsonConvert.DeserializeObject<Dictionary<string, HashSet<string>>>(Resource.dictionary);
+        static readonly Dictionary<string, HashSet<string>> Dictionary = JsonConvert.DeserializeObject<Dictionary<string, HashSet<string>>>(Resource.dictionary);
 
-        static void DictionaryLoader()
+        private static void DictionaryLoader()
         {
             // This method only needs to be ran once if a dictionary.json file isn't present 
             // Dictionary text file was taken from https://github.com/dwyl/english-words
 
             string line = "";
             Dictionary<string, HashSet<string>> words = new Dictionary<string, HashSet<string>>();
-            int counter = 0;
             StreamReader sr = new StreamReader(Resource.dictionaryText);
 
             // Reads through entire dictionary file, line by line and finds words that are between 5 and 9 characters long
             while ((line = sr.ReadLine()) != null)
             {
-                line.Trim();
-                if (5 <= line.Length && line.Length <= 9)
-                {
-                    // Saves a copy of the word for use in the dictionary hashset
-                    string word = line;
+                line = line.Trim();
+                if (5 > line.Length || line.Length > 9) 
+                    continue;
+                // Saves a copy of the word for use in the dictionary hashset
+                string word = line;
 
-                    // Orders the string alphabetically and does a check to see if that particular order of letters has appeared before
-                    // if it hasn't been seen before then a new hash set is created with the word as the initialising value.
-                    // If the letter ordering has been seen before, it is appended to the end of the set.
-                    line = string.Concat(line.OrderBy(c => c));
-                    if (!words.ContainsKey(line))
+                // Orders the string alphabetically and does a check to see if that particular order of letters has appeared before
+                // if it hasn't been seen before then a new hash set is created with the word as the initialising value.
+                // If the letter ordering has been seen before, it is appended to the end of the set.
+                line = string.Concat(line.OrderBy(c => c));
+                if (!words.ContainsKey(line))
+                {
+                    HashSet<string> wordSet = new HashSet<string>
                     {
-                        HashSet<string> wordSet = new HashSet<string>
-                        {
-                            word
-                        };
-                        words.Add(line, wordSet);
-                    }
-                    else
-                        words[line].Add(word);
+                        word
+                    };
+                    words.Add(line, wordSet);
                 }
-                counter++;
+                else
+                    words[line].Add(word);
             }
             sr.Close();
 
@@ -58,7 +55,7 @@ namespace CountdownWordFinder
             Console.WriteLine("Done!");
         }
 
-        static IEnumerable<IEnumerable<T>> CombinationFinder<T>(IEnumerable<T> items, int count)
+        private static IEnumerable<IEnumerable<T>> CombinationFinder<T>(IEnumerable<T> items, int count)
         {
             // Returns a list of all possible combinations of the items passed through.
             // The combinations will be 'count' long i.e if 2 is passed through, all 
@@ -84,19 +81,19 @@ namespace CountdownWordFinder
             //Console.Write("{");
             foreach (string i in set)
             {
-                Console.Write(" {0}", i);
+                Console.Write("{0}", i);
             }
             Console.WriteLine();
         }
 
-        static void WordFinder(string orderedLetters)
+        private static void WordFinder(string orderedLetters)
         {
             // Checks to see if the letter combination is a dictionary value and displays the hash set associated with it, if so.
-            if (dictionary.ContainsKey(orderedLetters))
-                DisplaySet(dictionary[orderedLetters]);
+            if (Dictionary.ContainsKey(orderedLetters))
+                DisplaySet(Dictionary[orderedLetters]);
         }
 
-        static void GetCombinations(List<string> letters)
+        private static void GetCombinations(List<char> letters)
         {
             // We're only interested in the 5-9 letter characters because anything else isn't really worth the time.
             for (int i = 5; i <= 9; i++)
@@ -118,14 +115,19 @@ namespace CountdownWordFinder
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter 9, space-seperated characters: ");
+            Console.WriteLine("Enter 9 characters");
             string l = Console.ReadLine();
 
-            List<string> letters = l.ToLower().Split().ToList();
+            if (l.Length < 9)
+                Console.WriteLine("Not enough characters entered!");
+            else
+            {
+                List<char> letters = new List<char>();
+                letters.AddRange(l);
 
-            GetCombinations(letters);
-            Console.WriteLine("Finished!");
-            
+                GetCombinations(letters);
+                Console.WriteLine("Finished!");
+            }
             Console.ReadKey();
         }
     }
